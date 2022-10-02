9e00cf61e47120e4492c4ecc2568eca7c82888c3
John Rhanzel <rhanzel1016@gmail.com>
2022-10-02 20:08:41 +0800

    update login
    Add mobile responsiveness and configure connection via php

--- login/login.php
+++ login/login.php
@@ -2,24 +2,13 @@
 include '../dbconnection.php';
 $conn = mysqli_connect("localhost", "root", "", "redlinedb");
 
-if (isset($_POST['login'])) {
-    $username = $_POST['username'];
-    $passwd = $_POST['passwd'];
+// define variables and set to empty values
+$username = $passwd = "";
+$usernameErr = $passwdErr = "";
 
-    $query = "SELECT * FROM admintb WHERE username='" . $_POST['username'] . "' AND passwd='" . $_POST['passwd'] . "'";
-    $result = mysqli_query($conn, $query);
-
-    if (mysqli_fetch_assoc($result)) {
-        $_SESSION['[username]'] = $_POST['username'];
-        $_SESSION['[passwd]'] = $_POST['passwd'];
-        // $_SESSION['pwd']=$_POST['pwd'];
-        header("location:../homepage/home.html"); //../foldername to include file on another folder in root
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($POST["username"])){
+        $usernameErr = "username is required";
     }
-    else {
-        header("location:../homepage/home.html? Invalid= Please enter correct username and password");
-    }
-}
-else {
-    echo 'Not Working';
 }
 ?>
